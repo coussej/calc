c6ceb0cc43bbd541fac7e38a4077d74691b3337d
Jeroen Coussement <jeroen.coussement@gmail.com>
2016-01-07 14:12:30 +0000

    add Abs, Pow

--- calc.go
+++ calc.go
@@ -1,5 +1,13 @@
 package calc
 
+// Abs return the absolute value of x
+func Abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
 // Min returns the smallest integer from its arguments.
 func Min(x ...int) (min int) {
 	for i, v := range x {
@@ -21,6 +29,25 @@ func Max(x ...int) (max int) {
 			max = v
 		case v > max:
 			max = v
+		}
+	}
+	return
+}
+
+// Pow returns x^y, the base-x exponential of y, with as condition y >= 0. If
+// y < 0, function returns 0.
+func Pow(x, y int) (pow int) {
+	switch {
+	case x == 0:
+		pow = 0
+	case y == 0:
+		pow = 1
+	case y < 0:
+		pow = 0
+	default:
+		pow = 1
+		for i := 0; i < y; i++ {
+			pow *= x
 		}
 	}
 	return
--- calc_test.go
+++ calc_test.go
@@ -6,18 +6,40 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestAbs(t *testing.T) {
+	a := Abs(1)
+	assert.Equal(t, 1, a, "Abs(1) should be 1")
+
+	a = Abs(-1)
+	assert.Equal(t, 1, a, "Abs(-1) should be 1")
+}
+
 func TestMin(t *testing.T) {
 	m := Min(1, 2, 3)
-	assert.True(t, m == 1, "Min of [1 2 3] should be 1")
+	assert.Equal(t, 1, m, "Min of [1 2 3] should be 1")
 
 	m = Min(-1, -2, -3)
-	assert.True(t, m == -3, "Min of [-1 -2 -3] should be -3")
+	assert.Equal(t, -3, m, "Min of [-1 -2 -3] should be -3")
 }
 
 func TestMax(t *testing.T) {
 	m := Max(1, 2, 3)
-	assert.True(t, m == 3, "Max of [1 2 3] should be 3")
+	assert.Equal(t, 3, m, "Max of [1 2 3] should be 3")
 
 	m = Max(-1, -2, -3)
-	assert.True(t, m == -1, "Max of [-1 -2 -3] should be -1")
+	assert.Equal(t, -1, m, "Max of [-1 -2 -3] should be -1")
+}
+
+func TestPow(t *testing.T) {
+	p := Pow(2, 4)
+	assert.Equal(t, 16, p, "2^2 should be 16")
+
+	p = Pow(-2, 3)
+	assert.Equal(t, -8, p, "-2^3 should be -8")
+
+	p = Pow(0, 1000)
+	assert.Equal(t, 0, p, "0^1000 should be 0")
+
+	p = Pow(1000, 0)
+	assert.Equal(t, 1, p, "1000^0 should be 0")
 }
