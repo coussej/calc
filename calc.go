6a404cb801ef8b3486913ce1facc13068b5434f7
Jeroen Coussement <jeroen.coussement@gmail.com>
2016-01-08 08:59:44 +0000

    Added some more funcs

--- calc.go
+++ calc.go
@@ -1,5 +1,7 @@
 package calc
 
+import "math"
+
 // Abs return the absolute value of x
 func Abs(x int) int {
 	if x < 0 {
@@ -8,17 +10,22 @@ func Abs(x int) int {
 	return x
 }
 
-// Min returns the smallest integer from its arguments.
-func Min(x ...int) (min int) {
-	for i, v := range x {
-		switch {
-		case i == 0:
-			min = v
-		case v < min:
-			min = v
-		}
+// Ceil returns the first integer higher than the given float64.
+func Ceil(x float64) int {
+	return int(math.Ceil(x))
+}
+
+// Copysign returns a value with the magnitude of x and the sign of y
+func Copysign(x, y int) int {
+	if y < 0 {
+		return -Abs(x)
 	}
-	return
+	return Abs(x)
+}
+
+// Floor returns the first integer lower than the given float64
+func Floor(x float64) int {
+	return int(math.Floor(x))
 }
 
 // Max returns the largest integer from its arguments.
@@ -34,16 +41,27 @@ func Max(x ...int) (max int) {
 	return
 }
 
+// Min returns the smallest integer from its arguments.
+func Min(x ...int) (min int) {
+	for i, v := range x {
+		switch {
+		case i == 0:
+			min = v
+		case v < min:
+			min = v
+		}
+	}
+	return
+}
+
 // Pow returns x^y, the base-x exponential of y, with as condition y >= 0. If
 // y < 0, function returns 0.
 func Pow(x, y int) (pow int) {
 	switch {
-	case x == 0:
+	case x == 0 || y < 0:
 		pow = 0
 	case y == 0:
 		pow = 1
-	case y < 0:
-		pow = 0
 	default:
 		pow = 1
 		for i := 0; i < y; i++ {
@@ -51,4 +69,9 @@ func Pow(x, y int) (pow int) {
 		}
 	}
 	return
+}
+
+// Round returns the correctly rounded int from the given float64 x
+func Round(x float64) int {
+	return Floor(x + .5)
 }
--- calc_test.go
+++ calc_test.go
@@ -14,12 +14,28 @@ func TestAbs(t *testing.T) {
 	assert.Equal(t, 1, a, "Abs(-1) should be 1")
 }
 
-func TestMin(t *testing.T) {
-	m := Min(1, 2, 3)
-	assert.Equal(t, 1, m, "Min of [1 2 3] should be 1")
+func TestCeil(t *testing.T) {
+	c := Ceil(3.25)
+	assert.Equal(t, 4, c, "Ceil(3.25) should be 4")
 
-	m = Min(-1, -2, -3)
-	assert.Equal(t, -3, m, "Min of [-1 -2 -3] should be -3")
+	c = Ceil(-3.25)
+	assert.Equal(t, -3, c, "Ceil(-3.25) should be -3")
+}
+
+func TestCopysign(t *testing.T) {
+	c := Copysign(12, -2)
+	assert.Equal(t, -12, c, "Copysign(12,-2) should be -12")
+
+	c = Copysign(-12, 2)
+	assert.Equal(t, 12, c, "Copysign(-12,2) should be 12")
+}
+
+func TestFloor(t *testing.T) {
+	f := Floor(3.25)
+	assert.Equal(t, 3, f, "Ceil(3.25) should be 4")
+
+	f = Floor(-3.25)
+	assert.Equal(t, -4, f, "Ceil(-3.25) should be -3")
 }
 
 func TestMax(t *testing.T) {
@@ -28,6 +44,14 @@ func TestMax(t *testing.T) {
 
 	m = Max(-1, -2, -3)
 	assert.Equal(t, -1, m, "Max of [-1 -2 -3] should be -1")
+}
+
+func TestMin(t *testing.T) {
+	m := Min(1, 2, 3)
+	assert.Equal(t, 1, m, "Min of [1 2 3] should be 1")
+
+	m = Min(-1, -2, -3)
+	assert.Equal(t, -3, m, "Min of [-1 -2 -3] should be -3")
 }
 
 func TestPow(t *testing.T) {
@@ -42,4 +66,21 @@ func TestPow(t *testing.T) {
 
 	p = Pow(1000, 0)
 	assert.Equal(t, 1, p, "1000^0 should be 0")
+}
+
+func TestRound(t *testing.T) {
+	r := Round(0.5)
+	assert.Equal(t, 1, r, "Round(0.5) should be 1")
+
+	r = Round(0.49999999999999)
+	assert.Equal(t, 0, r, "Round(0.49999999999999) should be 0")
+
+	r = Round(-0.5)
+	assert.Equal(t, 0, r, "Round(-0.5) should be 0")
+
+	r = Round(-0.49999999999999)
+	assert.Equal(t, 0, r, "Round(-0.49999999999999) should be 0")
+
+	r = Round(-0.50000000000001)
+	assert.Equal(t, -1, r, "Round(-0.50000000000001) should be -1")
 }
